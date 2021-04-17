9cc888971b8aff400b0424aa03a365ecf3e8e70d
markosanchez800 <markosanchez800@gmail.com>
2021-04-17 16:57:20 -0700

    intern extended class also included now

---
+++ intern.js
@@ -0,0 +1,17 @@
+const Employee = require('./employee')
+
+class Intern extends Employee{
+    constructor(name,id,email,school){
+        super(name,id,email);
+        this.school = school;
+    }
+
+    getSchool(){
+        return this.school;
+    }
+    getRole(){
+        return 'Intern';
+    }
+}
+
+module.exports = Intern;
--- manager.js
+++ manager.js
@@ -10,8 +10,17 @@ class Manager extends Employee {
         return this.officeNum;
     }
     getRole(){
+        console.log('Manager');
         return 'Manager';
     }
 }
+
+/*
+const Marko = new Manager('Marko',888,'markosanchez800@gmail.com',08);
+Marko.getName();
+Marko.getId();
+Marko.getEmail();
+Marko.getRole();
+*/
 
 module.exports = Manager;
