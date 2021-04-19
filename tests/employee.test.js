938969a437558ca5192394a2365953b7b422b311
markosanchez800 <markosanchez800@gmail.com>
2021-04-19 15:23:26 -0700

    all tests written for employee and they all pass

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -20,5 +20,33 @@ describe('Employee', () => {
         const testGuy = new Employee('Marko',888,email);
         expect(testGuy.email).toBe(email);
     })
+    describe('getName',()=>{
+        it('Should return the assigned name with function',()=>{
+            const testVal = 'Marko';
+            const testGuy = new Employee(testVal);
+            expect(testGuy.getName()).toBe(testVal);
+        });
+    });
+    describe('getId',()=>{
+        it('Should return the assigned id with function',()=>{
+            const testVal = 888;
+            const testGuy = new Employee('Marko',testVal);
+            expect(testGuy.getId()).toBe(testVal);
+        });
+    });
+    describe('getEmail',()=>{
+        it('Should return the assigned email with function',()=>{
+            const testVal = 'email@email.com';
+            const testGuy = new Employee('Marko',888,testVal);
+            expect(testGuy.getEmail()).toBe(testVal);
+        });
+    });
+    describe('getRole',()=>{
+        it('Should return the assigned role with function',()=>{
+            const testVal = 'Employee';
+            const testGuy = new Employee('Marko',888,'email@email.com');
+            expect(testGuy.getRole()).toBe(testVal);
+        })
+    })
 });
 
